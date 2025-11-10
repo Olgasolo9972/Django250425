aebe900426d62262171233f389e181a2f4442d76
Olgasolo9972 <olgasolo72@gmail.com>
2025-11-10 12:17:46 +0200

    Added changes according homework #7

--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpRequest, HttpResponse
+
+def hello_user(request: HttpRequest, user_name):
+    return HttpResponse(f"<h1>Hello, {user_name}!<h1>")
